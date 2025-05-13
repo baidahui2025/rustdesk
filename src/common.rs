3dc5781bf07bb8c9774cc0462ed0a00c91ff5ebd
baidahui2025 <zyfdntc@gmail.com>
2025-05-13 21:39:48 +0800

    gg,api

--- src/common.rs
+++ src/common.rs
@@ -990,7 +990,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://nas.zyfnas.asia:21114".to_owned()
 }
 
 #[inline]
